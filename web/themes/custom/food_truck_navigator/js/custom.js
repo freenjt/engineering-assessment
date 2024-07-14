53bc6b06693657ddbc1974342db4916546492592
freenjt <freenjt@hotmail.com>
2024-07-13 19:26:58 -0500

    Custom theme and configurations

---
+++ web/themes/custom/food_truck_navigator/js/custom.js
@@ -0,0 +1,18 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.food_truck_navigator = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+})(jQuery, Drupal);
